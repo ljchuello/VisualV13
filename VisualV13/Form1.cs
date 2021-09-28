b4d83234a0a30ca41f919d91e8a998983a99d249
Leonardo Chuello <ljchuello@gmail.com>
2021-09-28 10:23:16 -0500

    Haciendo layout

--- VisualV13/Form1.cs
+++ VisualV13/Form1.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MetroFramework.Forms;
 
 namespace VisualV13
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
